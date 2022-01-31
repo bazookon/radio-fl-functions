48c341beae5a3dd33378a21858df9f94d066bca8
bazookon <bazookon@gmail.com>
2022-01-31 02:23:06 +0000

    s

--- functions/src/controllers/getRadios/getRadios.ts
+++ functions/src/controllers/getRadios/getRadios.ts
@@ -1,4 +1,6 @@
 import * as functions from "firebase-functions";
+// Import cors
+import * as cors from "cors";
 
 import admin = require('firebase-admin');
 import { RadioModel } from "../../models/radio.model";
@@ -8,16 +10,20 @@ const db = admin.firestore();
 
 
 export const getRadios = functions.https.onRequest((req, res) => {
-    const radios: RadioModel[] = [];
-    db.collection('radios').get().then(snap => {
-        snap.forEach(doc => {
-            const radio: RadioModel = doc.data() as RadioModel;
-            radio.id = doc.id;
-            radios.push(radio);
+    cors({ origin: true })(req, res, () => {
+        const radios: RadioModel[] = [];
+        db.collection('radios').get().then(snap => {
+            snap.forEach(doc => {
+                const radio: RadioModel = doc.data() as RadioModel;
+                radio.id = doc.id;
+                radios.push(radio);
+            });
+            res.json({
+                radios: radios
+            });
         });
-        res.json({
-            radios: radios
-        });
-    });
+    })
+
+
 
 });
