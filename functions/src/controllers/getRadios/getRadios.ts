29f7027760c7ca647c2de3e33952ccf1a8fadb92
bazookon <bazookon@gmail.com>
2024-05-13 03:26:16 +0000

    chore: Update Node.js version to 18 and dependencies to latest stable versions

--- functions/src/controllers/getRadios/getRadios.ts
+++ functions/src/controllers/getRadios/getRadios.ts
@@ -1,29 +1,33 @@
-import * as functions from "firebase-functions";
-// Import cors
-import * as cors from "cors";
 
 import admin = require('firebase-admin');
 import { RadioModel } from "../../models/radio.model";
+import { onRequest } from "firebase-functions/v2/https";
 admin.initializeApp();
 
 const db = admin.firestore();
 
 
-export const getRadios = functions.https.onRequest((req, res) => {
-    cors({ origin: true })(req, res, () => {
-        const radios: RadioModel[] = [];
-        db.collection('radios').orderBy('name').where('isEnable', '==', true).get().then(snap => {
-            snap.forEach(doc => {
-                const radio: RadioModel = doc.data() as RadioModel;
-                radio.id = doc.id;
-                radios.push(radio);
-            });
-            res.json({
-                radios: radios,
-                country: req.headers["x-appengine-country"]
-            });
+export const getRadios = onRequest({
+    cors:
+        ['radio-fl.web.app']
+
+}, (req, res) => {
+    const radios: RadioModel[] = [];
+    db.collection('radios').orderBy('name').where('isEnable', '==', true).get().then(snap => {
+        snap.forEach(doc => {
+            const radio: RadioModel = doc.data() as RadioModel;
+            radio.id = doc.id;
+            radios.push(radio);
         });
-    })
+
+        // Cache control 300 seconds server cache 300 seconds browser cache
+        res.set('Cache-Control', 'public, max-age=300, s-maxage=300');
+
+        res.json({
+            radios: radios,
+            country: req.headers["x-appengine-country"]
+        });
+    });
 
 
 
